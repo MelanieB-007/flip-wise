f48bec66b253acca84f0f61f0938e617d0eae036
Melanie <git@melanie-busse.de>
2026-02-21 17:54:24 +0100

    1 - insert flashcards

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import Flashcard from "@/components/Flashcard";
+import styled from "styled-components";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+    return (
+        <CardContainer>
+            <Flashcard />
+            <Flashcard />
+            <Flashcard />
+        </CardContainer>
+    );
 }
+
+const CardContainer = styled.div `
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+  padding: 2rem;
+  justify-content: flex-start; 
+  align-content: flex-start;
+`;
