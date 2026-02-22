080b5044e71d75fd800cac0d3068dae7728be087
Melanie <git@melanie-busse.de>
2026-02-22 12:35:53 +0100

    0-Define the Mongoose Schema

---
+++ db/models/flashcard.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const flashcardSchema = new Schema({
+    collection: { type: String, required: true },
+    question: { type: String, required: true },
+    answer: { type: String, required: true },
+});
+
+const Flashcard = mongoose.models.Fashcard || mongoose.model("Flashcard", flashcardSchema);
+
+export default Flashcard;
