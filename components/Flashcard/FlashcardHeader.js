0845491e8861a3e371567720b290b0a65d918e8b
Melanie <git@melanie-busse.de>
2026-02-23 16:34:30 +0100

    1-insert icons for edit and delete

--- components/Flashcard/FlashcardHeader.js
+++ components/Flashcard/FlashcardHeader.js
@@ -1,20 +1,38 @@
 import styled from "styled-components";
+import EditIcon from "../Icons/EditIcon";
+import DeleteIcon from "../Icons/DeleteIcon";
 
-export default function FlashcardHeader({ color, category }) {
-  return (
-    <Header color={color}>
-      <Category>{category}</Category>
-    </Header>
-  );
+export default function FlashcardHeader({color, category, onEdit, onDelete}) {
+    return (
+        <Header color={color}>
+            <Category>{category}</Category>
+            <IconGroup>
+                <EditIcon onEdit={onEdit}/>
+                <DeleteIcon onDelete={onDelete}/>
+            </IconGroup>
+        </Header>
+    );
 }
 
 const Header = styled.div`
   background-color: ${({ color }) => color};
   padding: 0.8rem 1.2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const Category = styled.div`
   color: white;
   font-size: 1.8rem;
   font-weight: bold;
+  line-height: 1;
+`;
+
+const IconGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.4rem;
 `;
