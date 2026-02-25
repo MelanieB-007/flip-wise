c477b20a2587ef4dfdab540d705fd8b29143e36f
Melanie <git@melanie-busse.de>
2026-02-25 10:01:39 +0100

    6-Insert ListContainer

--- components/Flashcard/FlashcardList.js
+++ components/Flashcard/FlashcardList.js
@@ -1,11 +1,11 @@
 import Flashcard from "@/components/Flashcard/Flashcard";
-import styled from "styled-components";
 import Collapsible from "@/components/Collapsible";
 import FlashcardForm from "@/components/Flashcard/FlashcardForm";
+import ListContainer from "@/components/ListContainer"
 
 export default function FlashcardList({ flashcards, collections }) {
   return (
-    <FlashcardListContainer>
+    <ListContainer>
       <Collapsible label="+ Add Flashcard">
         {({ onClose }) => (
           <FlashcardForm collections={collections} onClose={onClose} />
@@ -20,22 +20,6 @@ export default function FlashcardList({ flashcards, collections }) {
           answer={flashcard.answer}
         />
       ))}
-    </FlashcardListContainer>
+    </ListContainer>
   );
 }
-
-const FlashcardListContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 1.5rem;
-  padding: 2rem;
-  justify-content: flex-start;
-  align-content: flex-start;
-  max-width: 900px;
-  margin: 0 auto;
-
-  @media (max-width: 768px) {
-    grid-template-columns: 1fr;
-    padding: 1rem;
-  }
-`;
