b5926355f8e4f074e8db4948cdde7c675f61f18e
Melanie <git@melanie-busse.de>
2026-03-01 00:06:38 +0100

    #11-bugfix

--- pages/api/collections/index.js
+++ pages/api/collections/index.js
@@ -2,35 +2,37 @@ import dbConnect from "../../../db/connect";
 import Collection from "@/db/models/collection";
 
 export default async function handler(request, response) {
-  await dbConnect();
+    await dbConnect();
 
-  if (request.method === "GET") {
-    try {
-      const collections = await Collection.find();
-      return response.status(200).json(collections);
-    } catch (error) {
-      return response.status(500).json({ error: error.message });
+    if (request.method === "GET") {
+        try {
+            const collections = await Collection.find().sort({createdAt: -1});
+            response.status(200).json(collections);
+            return;
+        } catch (error) {
+            response.status(500).json({error: error.message});
+            return;
+        }
     }
-  }
 
-  if (request.method === "POST") {
-    try {
-      const collectionData = request.body;
-      const collectionToCreate = await Collection.create(collectionData);
+    if (request.method === "POST") {
+        try {
+            const collectionData = request.body;
+            const collectionToCreate = await Collection.create(collectionData);
 
-      if (!collectionToCreate) {
-        response.status(404).json({ status: "Collection not found" });
-        return;
-      }
+            if (!collectionToCreate) {
+                response.status(404).json({status: "Collection not found"});
+                return;
+            }
 
-      response.status(201).json({
-        status: "Collection created.",
-      });
-    } catch (error) {
-      response.status(400).json({ error: error.message });
+            response.status(201).json({status: "Collection created."});
+            return;
+        } catch (error) {
+            response.status(400).json({error: error.message});
+            return;
+        }
     }
-  }
 
-  response.setHeader("Allow", ["GET", "POST"]);
-  return response.status(405).end(`Method ${request.method} not allowed`);
+    response.setHeader("Allow", ["GET", "POST"]);
+    return response.status(405).end(`Method ${request.method} not allowed`);
 }
