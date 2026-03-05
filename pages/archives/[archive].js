83d03a5118a0ac0cf5f8262da60d868218f3d9da
Ramon <ramon2@isbarn.net>
2026-03-05 11:39:22 +0100

    update archive page

--- pages/archives/[archive].js
+++ pages/archives/[archive].js
@@ -2,14 +2,18 @@ import FlashcardList from "@/components/Flashcard/FlashcardList";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import Headline from "@/components/Headline/Headline";
+import styled from "styled-components";
+import { useSession } from "next-auth/react";
 import {
   addColorToFlashcards,
-  getAnsweredFlashcards
+  getAnsweredFlashcards,
 } from "@/components/Service/FlashcardService";
 
 export default function CollectionArchive() {
   const router = useRouter();
   const { archive } = router.query;
+
+  const { data: session } = useSession();
 
   const {
     data: flashcards,
@@ -18,7 +22,7 @@ export default function CollectionArchive() {
   } = useSWR(`/api/flashcards`);
 
   const {
-    data: collections,
+    data: unfilteredCollections,
     isLoading: loadingCollections,
     error: errorCollections,
   } = useSWR(`/api/collections`);
@@ -30,15 +34,41 @@ export default function CollectionArchive() {
     return <div>Fehler beim Laden: {error.message} (Retry?)</div>;
   }
 
-  if (isLoading || !flashcards || !collections) {
+  if (isLoading || !flashcards || !unfilteredCollections) {
     return <h1>Loading...</h1>;
   }
 
-  const filteredFlashcards = getAnsweredFlashcards(
-      addColorToFlashcards(
-          flashcards.filter((flashcard) => flashcard.collection === archive),
-          collections)
+  const collections = session
+    ? unfilteredCollections.filter(
+        (collection) => collection.owner === session.user.id
+      )
+    : unfilteredCollections.filter(
+        (collection) => collection.owner === "default"
+      );
+
+  const currentCollection = collections.find(
+    (collection) => collection.name === archive
   );
+
+  if (!currentCollection) {
+    return (
+      <>
+        <Headline headline={"404"}></Headline>
+        <StyledErrorMessage>
+          No collection with the name {archive} found
+        </StyledErrorMessage>
+      </>
+    );
+  }
+
+  const flashcardsFromCollection = addColorToFlashcards(
+    flashcards.filter(
+      (flashcard) => flashcard.collection === currentCollection._id
+    ),
+    collections
+  );
+  const filteredFlashcards = getAnsweredFlashcards(flashcardsFromCollection);
+  const isEmpty = flashcardsFromCollection.length === 0;
 
   return (
     <>
@@ -52,3 +82,6 @@ export default function CollectionArchive() {
     </>
   );
 }
+const StyledErrorMessage = styled.p`
+  text-align: center;
+`;
